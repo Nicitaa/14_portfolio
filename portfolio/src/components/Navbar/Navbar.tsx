eed78ce22f3ec0a1d67358ad2411d79548c22969
Nicitaa <icpcsenondaryemail@gmail.com>
2023-04-27 19:49:43 +0200

    Navbar done

--- portfolio/src/components/Navbar/Navbar.tsx
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -1,20 +1,50 @@
 import './Navbar.css'
 import {repos} from '../../data/repos.ts'
 import {Link} from "react-router-dom";
+import {useEffect, useRef} from "react";
+
+
 
 export function Navbar(){
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const slider = sliderRef.current;
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    if (slider) {
+      slider.addEventListener("mousedown", (e) => {
+        isDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+
+      window.addEventListener("mouseup", () => {
+        isDown = false;
+      });
+
+      window.addEventListener("mousemove", (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const speed = 1;
+        const walk = (x - startX) * speed;
+        slider.scrollLeft = scrollLeft - walk;
+      });
+    }
+  }, []);
 return (
-  <nav className='nav'>
+
+  <nav ref={sliderRef} className='nav'>
     <div className='portfolio-container'>
     <Link to={'/'} className='portfolio'>Portfolio</Link>
     </div>
 
-
-
-
       {repos.map((repo)=>{
-        return <ul className='sites-ul'>
-           <li key={repo.id}>
+        return <ul key={repo.id} className='sites-ul'>
+           <li>
                    <p> {repo.id}</p>
                    <Link to={`/MySite/${repo.id}`}>{repo.description}</Link>
            </li>
@@ -22,5 +52,8 @@ return (
       })}
   </nav>
 
+
     )
+
 }
+
