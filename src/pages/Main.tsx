754dc1595aaabb8f9ed2ec6606df6a5e5cca0df8
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-24 13:56:48 +0200

    added animation to skill-progress

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import { skills } from "../data/skills";
 import './main.css'
 export function Main () {
 
+function Skill(skill:any) {
+  const [mounted,setMounted] = useState(false)
+
+useEffect(() => {
+  setMounted(true)
+},[])
+
+const width = mounted ? `${(skill.hours)/100+`%`}` : '0'
+
+return (
+<li key={skill.id}>
+      <h1>{skill.title}</h1>
+      <div className="progress-bar">
+        <div className="skill-progress" style={{width}}></div>
+      </div>
+    </li>
+)
+}
 
 
 return (
@@ -10,13 +29,7 @@ return (
   <ul>
   {skills.map(skill => {
     return (
-    <li key={skill.id}>
-      <h1>{skill.title}</h1>
-      <div className="progress-bar">
-        <div className="skill-progress" style={{width:(skill.hours)/100+`%`}}>
-        </div>
-      </div>
-    </li>
+    <Skill key={skill.id} {...skill}/>
     )
   })}
   </ul>
