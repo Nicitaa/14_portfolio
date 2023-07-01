821495f4e5576ab6ae126555f07d82ce269cfd34
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-01 20:39:56 +0200

    for save

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,48 +1,43 @@
 import './Navbar.css'
 import {repos} from './../../data/repos'
-import {Link} from "react-router-dom";
-import {useEffect, useRef} from "react";
+import {Link} from "react-router-dom"
+import {useEffect, useRef} from "react"
 
 
 
 export function Navbar(){
-  const sliderRef = useRef(null);
+  const sliderRef = useRef(null)
 
   useEffect(() => {
-    const slider = sliderRef.current;
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+    const slider = sliderRef.current
+    let isDown = false
+    let startX
+    let scrollLeft
 
     if (slider) {
       slider.addEventListener("mousedown", (e) => {
-      console.log('touched/swiped down')
-
-        isDown = true;
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-      });
+        isDown = true
+        startX = e.pageX - slider.offsetLeft
+        scrollLeft = slider.scrollLeft
+      })
 
       window.addEventListener("mouseup", () => {
-      console.log('touched/swiped up')
-        isDown = false;
-      });
+        isDown = false
+      })
 
       window.addEventListener("mousemove", (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        console.log('touched/swiped mousemove')
-
-        const x = e.pageX - slider.offsetLeft;
-        const speed = 1;
-        const walk = (x - startX) * speed;
-        slider.scrollLeft = scrollLeft - walk;
-      });
+        if (!isDown) return
+        e.preventDefault()
+        const x = e.pageX - slider.offsetLeft
+        const speed = 1
+        const walk = (x - startX) * speed
+        slider.scrollLeft = scrollLeft - walk
+      })
     }
-  }, []);
+  }, [])
 return (
 
-  <nav  className='nav'>
+  <nav className='nav'>
     <div className='portfolio-container'>
     <Link to={'/'} className='portfolio'>Portfolio</Link>
     </div>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,78 +4,71 @@ import './main.css'
 import gsap from "gsap";
 
 
-export function Main () {
+export function Main() {
 
-function Skill(skill:any) {
-  const progressRef:React.MutableRefObject<any> = useRef();
-  const hoursRef:React.MutableRefObject<any> = useRef();
-
-  
-  useLayoutEffect(() => {
-    const wn8 = 100/6
-    const percent = skill.hours / 100;
-    let background = '#fe0e00'
-    if (percent > wn8*0) {
-      background='#fe0e00' //red
-    }
-    if (percent > wn8*1) {
-      background='#fe7903' //orange
-    }
-    if (percent > wn8*2) {
-      background='#f8f403' //yellow
-    }
-    if (percent > wn8*3) {
-      background='#60ff00' //green
-    }
-    if (percent > wn8*4) {
-      background='#02c9b3' //turquoise
-    }
-    if (percent > wn8*5) {
-      background='#d042f3' //violet
-    }
+  function Skill(skill: any) {
+    const progressRef: React.MutableRefObject<any> = useRef()
+    const hoursRef: React.MutableRefObject<any> = useRef()
 
 
-  
-      gsap.timeline()
-        .to(progressRef.current, {
-          width: `${percent}%`,
-        })
-        .set(progressRef.current, {
-          background
-        },);
-    
-
-    // gsap.to(progressRef.current, {
-    //   width: `${percent}%`,
-    //   background,
-    // });
-
-
-  }, []);
-  
-return (
-<li key={skill.id}>
-      <h1>{skill.title}</h1>
-      <div className="progress-bar">
-        <div className="skill-progress" ref={progressRef}></div>
-      </div>
-      <h2 ref={hoursRef}>0</h2>
-    </li>
-)
-}
+    useLayoutEffect(() => {
+      const wn8 = 100 / 6
+      const percent = skill.hours / 100
+      let background = '#fe0e00'
+      if (percent > wn8 * 0) {
+        background = '#fe0e00' //red
+      }
+      if (percent > wn8 * 1) {
+        background = '#fe7903' //orange
+      }
+      if (percent > wn8 * 2) {
+        background = '#f8f403' //yellow
+      }
+      if (percent > wn8 * 3) {
+        background = '#60ff00' //green
+      }
+      if (percent > wn8 * 4) {
+        background = '#02c9b3' //turquoise
+      }
+      if (percent > wn8 * 5) {
+        background = '#d042f3' //violet
+      }
 
 
-return (
-<div className="main">
-<div className="skills">
-  <ul>
-  {skills.map(skill => {
+
+
+
+      gsap.to(progressRef.current, {
+        width: `${percent}%`,
+        background,
+      })
+
+
+    }, [])
+
     return (
-    <Skill key={skill.id} {...skill}/>
+      <li key={skill.id}>
+        <h1>{skill.title}</h1>
+        <div className="progress-bar">
+          <div className="skill-progress" ref={progressRef}></div>
+        </div>
+        <h2 ref={hoursRef}>0</h2>
+      </li>
     )
-  })}
-  </ul>
-</div>
-</div>
-)
+  }
+
+
+  return (
+    <div className="main">
+      <div className="skills">
+        <ul>
+          {skills.map(skill => {
+            return (
+              <Skill key={skill.id} {...skill} />
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
 }
