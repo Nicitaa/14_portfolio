616af5545537201b116f7178bec3ab20f6286821
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-29 14:15:18 +0200

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
 import { Main } from "./pages"
 import { Footer } from "./components/Footer"
+import { Page404 } from "./pages/Page404"
 
 function App() {
 
   return (
-    <div className="font-primary text-secondary-dark text-sm">
+    <div className="font-primary text-secondary-dark text-sm flex flex-col min-h-[100vh]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/*" element={<Page404 />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { RiDiscordLine } from "react-icons/ri";
 
 export function Footer() {
   return (
-    <footer className="mb-lg">
+    <footer className="mb-lg mt-auto">
       <div className="flex justify-center gap-x-md">
         <Button onClick={() => window.open('https://t.me/icpcedu')}>Telegram <PiTelegramLogoBold /></Button>
         <Button onClick={() => window.open('https://discord.com/users/780002958380498955')}>Discord <RiDiscordLine /></Button>
