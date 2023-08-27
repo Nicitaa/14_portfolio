1062f69ae8374af0d61454c973735145c923e5d6
Nicitaa <icpcsenondaryemail@gmail.com>
2023-08-27 21:02:48 +0200

    main-imp-footerAddSummary

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,22 @@
-import { PiTelegramLogoBold } from "react-icons/pi";
-import { Button } from "./Button";
-import { RiDiscordLine } from "react-icons/ri";
+import { PiTelegramLogoBold } from "react-icons/pi"
+import { RiDiscordLine } from "react-icons/ri"
+import {AiFillLinkedin} from 'react-icons/ai'
+import {GrDocumentPdf} from 'react-icons/gr'
+
+import { Button } from "./Button"
 
 export function Footer() {
+
+
   return (
     <footer className="mb-lg mt-auto">
-      <div className="flex justify-center gap-x-md">
+      <div className="relative grid grid-cols-2 tablet:flex gap-y-md max-w-[80vw] mx-auto justify-center gap-x-md">
         <Button onClick={() => window.open('https://t.me/nicitaacom')}>Telegram <PiTelegramLogoBold /></Button>
         <Button onClick={() => window.open('https://discord.com/users/780002958380498955')}>Discord <RiDiscordLine /></Button>
+        <Button onClick={() => window.open('https://linkedin.com/in/nicitaa')}>LinkedIn <AiFillLinkedin /></Button>
+        <Button onClick={() => window.open('https://drive.google.com/file/d/1veZDYS_i1Udmy_a-zV4c55cOABTglIPa/view?usp=sharing')}>
+          Summary<GrDocumentPdf/>
+        </Button>
       </div>
     </footer>
   )
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
-import useModalMoreInfo15 from "./useModalMoreInfo15";
-import useModalMoreInfo16 from "./useModalMoreInfo16";
-import useModalMoreInfo22 from "./useModalMoreInfo22";
+import useNavbar from "./useNavbar";
+import {useSlider} from "./useSlider";
 
-export {useModalMoreInfo15,useModalMoreInfo16,useModalMoreInfo22}
+
+export {useNavbar,useSlider}
