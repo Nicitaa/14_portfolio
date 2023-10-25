3fd1f2a708bc65905d9a741848587ee94cbb6a81
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-25 14:02:17 +0200

    footer created

---
+++ app/components/Footer.tsx
@@ -0,0 +1,29 @@
+import { PiTelegramLogoBold } from "react-icons/pi"
+import { RiDiscordLine } from "react-icons/ri"
+import { AiFillLinkedin } from "react-icons/ai"
+import { BsFiletypePdf } from "react-icons/bs"
+
+import { Button } from "./Button"
+
+export function Footer() {
+  return (
+    <footer className="mb-lg mt-auto">
+      <div className="relative grid grid-cols-2 tablet:flex gap-y-md max-w-[80vw] mx-auto justify-center gap-x-md">
+        <Button href="https://t.me/nicitaacom" target="_blank">
+          Telegram <PiTelegramLogoBold />
+        </Button>
+        <Button href="https://discord.com/users/780002958380498955" target="_blank">
+          Discord <RiDiscordLine />
+        </Button>
+        <Button href="https://linkedin.com/in/nicitaa" target="_blank">
+          LinkedIn <AiFillLinkedin />
+        </Button>
+        <Button
+          href="https://drive.google.com/file/d/1veZDYS_i1Udmy_a-zV4c55cOABTglIPa/view?usp=sharing"
+          target="_blank">
+          Summary <BsFiletypePdf />
+        </Button>
+      </div>
+    </footer>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Layout from "./components/Layout"
 import { Navbar } from "./components/Navbar"
+import { Footer } from "./components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <Navbar />
         <Layout>{children}</Layout>
+        <Footer />
       </body>
     </html>
   )
