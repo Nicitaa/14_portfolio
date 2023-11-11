0b2e0ea088ec299a01f9f09744d1c2bda4729ea6
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-11 13:57:48 +0100

    chore: TypeScript Intellisense improved
    Now I don't need to write generic between  each closeModal and ()

--- app/store/modalsStore.ts
+++ app/store/modalsStore.ts
@@ -1,9 +1,10 @@
+import { TModals } from "@/interfaces/TModals"
 import { create } from "zustand"
 
 interface ModalsStore {
   isOpen: Record<string, boolean>
-  openModal: <T extends string>(id: T) => void
-  closeModal: <T extends string>(id: T) => void
+  openModal: <T extends TModals>(id: T) => void
+  closeModal: <T extends TModals>(id: T) => void
 }
 
 export const useModalsStore = create<ModalsStore>()(set => ({
