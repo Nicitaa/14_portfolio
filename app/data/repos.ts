f7e85f5d8f526628d70a9519993bf9153dcabd7a
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-12-01 09:11:13 +0100

    +-upd: navbar

--- app/data/repos.ts
+++ app/data/repos.ts
@@ -2,133 +2,127 @@ const repos = [
   {
     id: 1,
     description: "influencers",
-    url: "https://github.com/Nicitaa/MyFirstSite"
+    url: "https://github.com/Nicitaa/MyFirstSite",
   },
   {
     id: 2,
     description: "spaklevka",
-    url: "https://github.com/Nicitaa/MySecondSite"
+    url: "https://github.com/Nicitaa/MySecondSite",
   },
   {
     id: 3,
     description: "bootstrap net ninja",
-    url: "https://github.com/Nicitaa/MyThirdSite"
+    url: "https://github.com/Nicitaa/MyThirdSite",
   },
   {
     id: 4,
     description: "go trip",
-    url: "https://github.com/Nicitaa/MyFourthSite"
+    url: "https://github.com/Nicitaa/MyFourthSite",
   },
   {
     id: 5,
     description: "netflix clone",
-    url: "https://github.com/Nicitaa/MyFifthSite"
+    url: "https://github.com/Nicitaa/MyFifthSite",
   },
   {
     id: 6,
     description: "invoriem",
-    url: "https://github.com/Nicitaa/MySixthSite"
+    url: "https://github.com/Nicitaa/MySixthSite",
   },
   {
     id: 7,
     description: "navbars forms",
-    url: "https://github.com/Nicitaa/MySeventhSite"
+    url: "https://github.com/Nicitaa/MySeventhSite",
   },
   {
     id: 8,
     description: "accordeon pop-up",
-    url: "https://github.com/Nicitaa/MyEighthSite"
+    url: "https://github.com/Nicitaa/MyEighthSite",
   },
   {
     id: 9,
     description: "project",
-    url: "https://github.com/Nicitaa/MyNinethSite"
+    url: "https://github.com/Nicitaa/MyNinethSite",
   },
   {
     id: 10,
     description: "product",
-    url: "https://github.com/Nicitaa/MyTenthSite"
+    url: "https://github.com/Nicitaa/MyTenthSite",
   },
   {
     id: 11,
     description: "githubApi",
-    url: "https://github.com/Nicitaa/MyEleventhSite"
+    url: "https://github.com/Nicitaa/MyEleventhSite",
   },
   {
     id: 12,
     description: "shopping cart",
-    url: "https://github.com/Nicitaa/MyTwelvthSite"
+    url: "https://github.com/Nicitaa/MyTwelvthSite",
   },
   {
     id: 13,
     description: "to-do",
-    url: "https://github.com/Nicitaa/MyirteenthSite"
+    url: "https://github.com/Nicitaa/MyirteenthSite",
   },
   {
     id: 14,
     description: "portfolio",
-    url: "https://github.com/Nicitaa/14_portfolio"
+    url: "https://github.com/Nicitaa/14_portfolio",
   },
   {
     id: 15,
     description: "HooBank",
-    url: "https://github.com/Nicitaa/15_HooBank"
+    url: "https://github.com/Nicitaa/15_HooBank",
   },
   {
     id: 16,
     description: "gericht-restaurant",
-    url: "https://github.com/Nicitaa/16_gericht-restaurant"
+    url: "https://github.com/Nicitaa/16_gericht-restaurant",
   },
   {
     id: 17,
-    description: "airbnb clone",
-    url: "https://github.com/Nicitaa/17_Airbnb-clone"
+    description: "messenger clone",
+    url: "https://github.com/nicitaacom/17_messenger-clone",
   },
   {
     id: 18,
     description: "utilities",
-    url: "https://github.com/Nicitaa/18_utilities"
+    url: "https://github.com/Nicitaa/18_utilities",
   },
   {
     id: 19,
     description: "spotify clone",
-    url: "https://github.com/Nicitaa/19_spotify-clone"
+    url: "https://github.com/Nicitaa/19_spotify-clone",
   },
   {
     id: 20,
-    description: "?",
-    url: "https://github.com/Nicitaa/"
+    description: "flowmazon clone",
+    url: "https://github.com/nicitaacom/20_flowmazon-clone",
   },
   {
     id: 21,
     description: "auth-form (FullStack)",
-    url: "https://github.com/Nicitaa/21_auth-form"
+    url: "https://github.com/Nicitaa/21_auth-form",
   },
   {
     id: 22,
     description: "aer",
-    url: "https://github.com/Nicitaa/22_aer"
+    url: "https://github.com/Nicitaa/22_aer",
   },
-
-    {
+  {
     id: 23,
     description: "23_store",
-    url: "https://github.com/Nicitaa/23_store-zustand-supabase"
+    url: "https://github.com/nicitaacom/23_store",
   },
-    {
+  {
     id: 24,
     description: "dashboard MUI",
-    url: "https://github.com/Nicitaa/24_dashboard-mui"
+    url: "https://github.com/Nicitaa/24_dashboard-mui",
   },
-    {
+  {
     id: 25,
     description: "bottle energy",
-    url: "https://github.com/Nicitaa/25_bottle-energy"
-  },
-    {
-    id: 26,
-    description: "linktree clone",
-    url: "https://github.com/Nicitaa/26_linktree-clone"
+    url: "https://github.com/Nicitaa/25_bottle-energy",
   },
 ]
 
