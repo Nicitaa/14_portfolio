4fbace5e9bdf2ec2894782511d7094283c6894b8
Nikita <nicitaacom@gmail.com>
2024-03-08 17:12:30 +0100

    update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Skill } from "@/components/Skill"
 import { TooltipOther, TooltipReact } from "../components/Tooltips"
 import { ProjectsSwitcher } from "@/components/ProjectsSwitcher"
@@ -9,8 +11,7 @@ export default function Home() {
       {/* Skills + Text */}
       <div
         className="flex flex-col desktop:flex-row justify-between items-center gap-lg w-full px-sm
-      tablet:px-md
-      desktop:max-w-[80%] desktop:h-[40rem]">
+      tablet:px-md desktop:max-w-[80%] desktop:h-[40rem]">
         <ul className="flex flex-col gap-y-sm">
           <Skill id={1} label="html&css" hours={3000} />
           <Skill id={2} label="React" hours={2656} tooltip tooltiptext={<TooltipReact />} />
@@ -18,10 +19,8 @@ export default function Home() {
           <Skill id={4} label="Next" hours={1470} />
           <Skill id={5} label="other" hours={1103} tooltip tooltiptext={<TooltipOther />} />
         </ul>
-        <div className="tablet:px-md text-center desktop:max-w-[650px] desktop:px-[0] desktop:text-start">
-          <h1
-            data-text="WEB Frontend developer"
-            className="text-shadow text-lg min-[380px]:whitespace-nowrap before:text-secondary">
+        <div className="text-center w-fit break-words max-w-[650px] desktop:px-[0] desktop:text-start">
+          <h1 data-text="WEB Frontend developer" className="text-shadow text-lg before:text-secondary">
             Middle FullStack WEB developer
           </h1>
           <div className="text-md font-bold text-secondary-foreground">
@@ -33,7 +32,10 @@ export default function Home() {
             <i>school/colledge/academy</i> don&apos;t help you to get success in your life - that&apos;s why you need
             invest your free time in vision in your head of how to get success.
             <br />
-            <b>Soon I</b> become a Senior.
+            <b>Soon I</b> become a Senior. Book call with me&nbsp;
+            <Link className="text-cta" href="/appointment">
+              here
+            </Link>
           </div>
         </div>
       </div>
