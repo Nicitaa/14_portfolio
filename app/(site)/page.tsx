2a8c534f34659a57229695e29f4641c3d0519d36
Nikita <nicitaacom@gmail.com>
2024-03-09 09:08:53 +0100

    upd: main text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -26,13 +26,50 @@ export default function Home() {
           <div className="text-md font-bold text-secondary-foreground">
             <b>I&apos;m started</b> my career in 2018 and tried 4 different programming languages.
             <br />
-            Right now my main stack - Next + TypeScript + Tailwind
+            <b>My main stack</b> - Next + TypeScript + Tailwind
             <br />
-            <b>I dropped</b> my academy and want to say people in young age that knowlege that you get in&nbsp;
+            <b>I&apos;m creating</b> websites with high{" "}
+            <div className="relative inline-block">
+              <span data-text="conversion" className="text-tooltip tooltip">
+                <div className="tooltiptext">
+                  <h1 className="text-sm tablet:text-xs whitespace-nowrap">
+                    When 100 people visit your site
+                    <br /> and only 1 of them buy smth
+                    <br className="tablet:hidden" /> - it means conversion rate = 1%
+                  </h1>
+                </div>
+              </span>
+            </div>{" "}
+            rate and amazing UI/UX/CX
+            <br />
+            Here is&nbsp;
+            <div className="relative inline-block">
+              <span data-text="message" className="text-tooltip tooltip">
+                <div className="tooltiptext">
+                  <h2 className="text-sm tablet:text-xs">
+                    <b>I dropped</b> my academy and want
+                    <br className="tablet:hidden" /> to say people
+                    <br className="hidden tablet:block" /> in young age that knowlege
+                    <br className="tablet:hidden" /> that you get in&nbsp;
+                    <br className="hidden tablet:block" />
+                    <i>school/colledge/academy</i>
+                    <br className="tablet:hidden" /> don&apos;t help you to get <br className="hidden tablet:block" />
+                    success in your life
+                    <br className="tablet:hidden" /> - that&apos;s why you need invest your
+                    <br /> free time in vision in your head
+                    <br className="tablet:hidden" /> of how to get success.
+                    <br />
+                  </h2>
+                </div>
+              </span>
+            </div>
+            &nbsp; I want to say
+            <br />
+            {/* <b>I dropped</b> my academy and want to say people in young age that knowlege that you get in&nbsp;
             <i>school/colledge/academy</i> don&apos;t help you to get success in your life - that&apos;s why you need
             invest your free time in vision in your head of how to get success.
-            <br />
-            <b>Soon I</b> become a Senior. Book call with me&nbsp;
+            <br /> */}
+            <b> Book call</b> with me&nbsp;
             <Link className="text-cta" href="/appointment">
               here
             </Link>
