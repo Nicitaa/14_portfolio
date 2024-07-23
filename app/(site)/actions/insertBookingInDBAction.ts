af7163e50cc815b9213db89d332ef6f0bf895f20
Nikita <nicitaacom@gmail.com>
2024-07-23 09:47:07 +0200

    upd: toast error

--- app/(site)/actions/insertBookingInDBAction.ts
+++ app/(site)/actions/insertBookingInDBAction.ts
@@ -6,6 +6,7 @@ import { nanoid } from "nanoid"
 import { Channel } from "@/store/useAppointmentStore"
 import { Value } from "@/store/useSelectedDateStore"
 import { rateLimit } from "@/libs/rateLimit"
+import supabaseAdmin from "@/libs/supabaseAdmin"
 
 export async function insertBookingInDBAction(selectedDate: Value, at: string, channel: Exclude<Channel, null>) {
   const userCookieId = cookies().get("user_cookie_id")?.value || nanoid()
@@ -16,43 +17,40 @@ export async function insertBookingInDBAction(selectedDate: Value, at: string, c
 
   // Rate limit checks
 
-  throw new Error("You have already booked a call. Please try again tomorrow.")
   const { success } = await rateLimit(ip, 1, 86400)
-  console.log(21, "success - ", success)
 
-  throw new Error("You have already booked a call today. Please try again tomorrow.")
   if (!success) {
     throw new Error("You have already booked a call today. Please try again tomorrow.")
   }
 
-  // // to avoid error with type string in DB and type Value here
-  // if (Array.isArray(selectedDate)) {
-  //   if (selectedDate.length > 0 && selectedDate[0] instanceof Date) {
-  //     // If selectedDate is an array of Date objects, use the first one and convert it to a string
-  //     bookingDate = (selectedDate[0] as Date).toISOString().split("T")[0]
-  //   } else {
-  //     throw new Error("Invalid selectedDate")
-  //   }
-  // } else if (typeof selectedDate === "object" && selectedDate !== null && selectedDate instanceof Date) {
-  //   // If selectedDate is a Date object, convert it to a string
-  //   bookingDate = selectedDate.toISOString().split("T")[0]
-  // } else {
-  //   // Handle other cases, like null or unknown
-  //   throw new Error("Invalid selectedDate")
-  // }
+  // to avoid error with type string in DB and type Value here
+  if (Array.isArray(selectedDate)) {
+    if (selectedDate.length > 0 && selectedDate[0] instanceof Date) {
+      // If selectedDate is an array of Date objects, use the first one and convert it to a string
+      bookingDate = (selectedDate[0] as Date).toISOString().split("T")[0]
+    } else {
+      throw new Error("Invalid selectedDate")
+    }
+  } else if (typeof selectedDate === "object" && selectedDate !== null && selectedDate instanceof Date) {
+    // If selectedDate is a Date object, convert it to a string
+    bookingDate = selectedDate.toISOString().split("T")[0]
+  } else {
+    // Handle other cases, like null or unknown
+    throw new Error("Invalid selectedDate")
+  }
 
-  // const { error } = await supabaseAdmin.from("bookings").insert({
-  //   id: nanoid(),
-  //   booking_date: bookingDate,
-  //   booking_time_MSK: at,
-  //   channel: channel,
-  //   user_cookie_id: userCookieId,
-  // })
+  const { error } = await supabaseAdmin.from("bookings").insert({
+    id: nanoid(),
+    booking_date: bookingDate,
+    booking_time_MSK: at,
+    channel: channel,
+    user_cookie_id: userCookieId,
+  })
 
-  // // await redis.set(cookies().get('user_cookie_id'))
+  // await redis.set(cookies().get('user_cookie_id'))
 
-  // if (error) {
-  //   console.error(40, "Error inserting booking:", error)
-  //   throw error
-  // }
+  if (error) {
+    console.error(40, "Error inserting booking:", error)
+    throw error
+  }
 }
