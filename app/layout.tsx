f5c3bc441e349ad20ffcb5785c898771d9076556
Nikita <nicitaacom@gmail.com>
2024-12-02 04:49:22 +0100

    chore: GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google"
 
 import Layout from "./components/Layout"
 import { Navbar } from "./components/Navbar/Navbar"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"], preload: true })
 
@@ -16,7 +17,31 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Add Google Tag Manager script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','AW-11303856122');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
+        <noscript>
+          {/* Fallback for browsers with disabled JavaScript */}
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=AW-11303856122"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <Navbar />
         <Layout>{children}</Layout>
       </body>
