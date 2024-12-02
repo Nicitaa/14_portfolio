e6e9678ead016d134faa6e9b7e6337d154b03fde
Nikita <nicitaacom@gmail.com>
2024-12-02 12:33:51 +0100

    upd: initial time logic
    // if now 19:23 - show 20:00
    // if now 19:44 - show 20:30
    // if now 20:00 - show 10:00
    // if now 8:40 - show 10:00
    // if now 9:30 - show 10:30
    // if now 9:29 - show 10:00

--- app/store/useSelectedTimeStore.ts
+++ app/store/useSelectedTimeStore.ts
@@ -15,13 +15,15 @@ const currentMinute = new Date().getMinutes()
 const nextHalfHour = currentMinute < 30 ? 30 : 0
 const nextHour = currentMinute < 30 ? currentHour : currentHour + 1
 
-const initialHour = nextHour < 10 ? 10 : nextHour
-const initialMinute = nextHour < 10 ? 0 : nextHalfHour
+const initialHour = currentHour >= 20 ? 10 : nextHour < 10 ? 10 : nextHour
+const initialMinute = currentHour >= 20 ? 0 : nextHour < 10 ? 0 : nextHalfHour
 
-// if now 19:23 - show 19:30
-// if now 19:44 - show 20:00
+// if now 19:23 - show 20:00
+// if now 19:44 - show 20:30
 // if now 20:00 - show 10:00
 // if now 8:40 - show 10:00
+// if now 9:30 - show 10:30
+// if now 9:29 - show 10:00
 const initialTime = `${String(initialHour).padStart(2, "0")}:${String(initialMinute).padStart(2, "0")}`
 
 export const useSelectedTimeStore = create<SelectedTimeStore>()(set => ({
